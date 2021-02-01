974b19d3210125d7d5a0c0427e61d385be5c166a
Lailz <lailaabdulraheem@gmail.com>
2021-02-01 08:36:13 +0300

    not found

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
 import instructors from "./data";
 
 const theme = {
@@ -26,10 +28,12 @@ const GlobalStyle = createGlobalStyle`
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <App instructors={instructors} />
-    </ThemeProvider>
+    <BrowserRouter>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <App instructors={instructors} />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
